17cf46d9ea26a4e7feb57e4db1fe05016e7f6905
gunapermana <100502223+gunapermana@users.noreply.github.com>
2023-03-24 23:39:16 +0800

    Add files via upload

---
+++ src/routes.js
@@ -0,0 +1,28 @@
+const {
+  addBookHandler, getAllBooksHandler, getBookByIdHandler, editBookByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: editBookByIdHandler,
+  },
+];
+
+module.exports = routes;
